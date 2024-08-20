270982ce014979e036aae42a0136fde53cf358d3
Alexey Russkikh <russkikhh@yandex.ru>
2024-08-20 19:42:50 +0400

    Добавил выполненые задания 1-3 лекции №6;

--- src/main/java/lesson_6/Person.java
+++ src/main/java/lesson_6/Person.java
@@ -1,12 +1,12 @@
 package lesson_6;
 
 public class Person {
-    private final String fullName;
-    private final String jobTitle;
-    private final String eMail;
-    private final String telephoneNumber;
-    private final int salary;
-    private final int age;
+    private String fullName;
+    private String jobTitle;
+    private String eMail;
+    private String telephoneNumber;
+    private int salary;
+    private int age;
 
     public Person(String fullName, String jobTitle, String eMail, String telephoneNumber, int salary, int age) {
         this.fullName = fullName;
